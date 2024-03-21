68405f1f6520356677ca72d464f22d50317692f0
Rafael Andrade <rafael.io@outlook.com>
2024-03-21 19:51:44 -0300

    Finaliza projeto front end

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <div class="card">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div> -->
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/main.scss";
+</style>
